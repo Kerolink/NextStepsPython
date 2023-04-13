d5ac56431405cdfdb82dbc2eb41d92caa8e6b4a5
JonPiniesEcheguren <jon.pinies.ech@gmail.com>
2023-04-13 11:16:20 +0200

    Ejers Jon

---
+++ Ejercicio7_11Python.py
@@ -0,0 +1,7 @@
+# 7. Crea un programa que le pida al usuario una cadena de caracteres y luego imprima cada carácter en una línea separada.
+# El programa debe utilizar un bucle `for` para recorrer la cadena.
+print("Introduce una cadena de caracteres:")
+cadena = input()
+for letra in cadena:
+    print(letra)
+print("El programa ha finalizado")
---
+++ Ejercicio8_11Python.py
@@ -0,0 +1,14 @@
+# 8. Crea un programa en Python que acepte una lista de cadenas de caracteres y devuelva una nueva lista que contenga solo las cadenas que tienen más de 5 caracteres. 
+# El programa debe utilizar un bucle `for` para recorrer la lista y una estructura de control de flujo para filtrar las cadenas.
+cadenas = list()
+cadcinco = list()
+palabra = "aleatorio"
+while len(palabra)>1:
+    print("Introduzca una nueva cadena de caracteres")
+    palabra = input()
+    cadenas.append(palabra)
+for pal in cadenas:
+    if len(pal)>5:
+        cadcinco.append(pal)
+print(cadcinco)
+print("El programa ha finalizado")
