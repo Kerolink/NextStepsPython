35017f1a0c8769105b5e9fb5b84003ffbccc6a8a
JonPiniesEcheguren <jon.pinies.ech@gmail.com>
2023-04-13 11:22:00 +0200

    ejers con función principal

--- Ejercicio1_11Python.py
+++ Ejercicio1_11Python.py
@@ -1,16 +1,17 @@
 # 1. Crea un programa en Python que acepte una fecha como cadena de caracteres en formato `"dd/mm/aaaa"` y devuelva la fecha en formato `"aaaa-mm-dd"`, con el año primero. 
 # El programa debe manejar excepciones en caso de que la cadena no tenga el formato correcto.
-from datetime import datetime
-print("Introduzca una fecha en formato dd/mm/aaaa:")
-fecha = input()
-x = 1
-while x == 1:
-  try:
-    obj = datetime.strptime(fecha, "%d/%m/%Y").date()
-    print(f"{obj.year}-{obj.month}-{obj.day}")
-    x = 0 
-  except:
-    print("La fecha introducida es errónea")
-    print("Introduzca una fecha en formato dd/mm/aaaa:")
-    fecha = input()
-print("El programa ha finalizado")
+def principal():
+  from datetime import datetime
+  print("Introduzca una fecha en formato dd/mm/aaaa:")
+  fecha = input()
+  x = 1
+  while x == 1:
+    try:
+      obj = datetime.strptime(fecha, "%d/%m/%Y").date()
+      print(f"{obj.year}-{obj.month}-{obj.day}")
+      x = 0 
+    except:
+      print("La fecha introducida es errónea")
+      print("Introduzca una fecha en formato dd/mm/aaaa:")
+      fecha = input()
+  print("El programa ha finalizado")
--- Ejercicio2_11Python.py
+++ Ejercicio2_11Python.py
@@ -1,14 +1,15 @@
 # 2. Crea un programa que le pregunte al usuario su zona horaria y le muestre la hora actual en esa zona. 
 # El programa debe manejar excepciones en caso de que la zona horaria ingresada no sea válida.
-from datetime import datetime, timedelta
-from pytz import timezone
-import pytz
+def principal():
+        from datetime import datetime, timedelta
+        from pytz import timezone
+        import pytz
 
-print("Selecciona tu zona horaria de entre las siguientes:")
-print(pytz.all_timezones)
-zona = input ()
-while zona not in pytz.all_timezones:
-        print("La zona horaria introducida no es válida")
+        print("Selecciona tu zona horaria de entre las siguientes:")
+        print(pytz.all_timezones)
         zona = input ()
-print(f"Hora en {zona}: {datetime.now(pytz.timezone(zona)).hour}:{datetime.now(pytz.timezone(zona)).minute}")
-print("El programa ha finalizado")
+        while zona not in pytz.all_timezones:
+                print("La zona horaria introducida no es válida")
+                zona = input ()
+        print(f"Hora en {zona}: {datetime.now(pytz.timezone(zona)).hour}:{datetime.now(pytz.timezone(zona)).minute}")
+        print("El programa ha finalizado")
